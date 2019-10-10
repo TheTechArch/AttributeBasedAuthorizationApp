176a40ff73e7c454fdab495e4217a73d1d03dfa4
Rune T. Larsen <rune@hjemmekino.no>
2019-10-10 07:46:05 +0200

    Just somce code

---
+++ AttributeBasedAuthorizationApp/AttributeBasedAuthorizationApp/Authorization/DataAccessHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeBasedAuthorizationApp.Authorization
+{
+
+
+    public class DataAccessHandler: AuthorizationHandler<DataAccessRequirement>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DataAccessHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, DataAccessRequirement requirement)
+        {
+
+            HttpContext.User.
+
+            _httpContextAccessor.HttpContext.GetRouteData();
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AttributeBasedAuthorizationApp/AttributeBasedAuthorizationApp/Authorization/DataAccessRequirement .cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeBasedAuthorizationApp.Authorization
+{
+    public class DataAccessRequirement: IAuthorizationRequirement
+    {
+
+        public string Action { get;  }
+
+        public DataAccessRequirement(string action)
+        {
+
+        }
+
+    }
+}
